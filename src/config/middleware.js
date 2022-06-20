7b411e34a36f0410174ca97b425a2587b9874ee3
Akshay Chaturvedi <akshay.chaturvedi@vaahaq.com>
2022-06-20 18:36:39 +0530

    change path

--- src/config/middleware.js
+++ src/config/middleware.js
@@ -1,23 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 
-const allowedDomains = ['http://localhost:3000'];
+// const allowedDomains = ['http://localhost:3000'];
 
 export default app => {
   app.use(express.json());
-  app.use(
-    cors({
-      credentials: true,
-      origin: (origin, callback) => {
-        // bypass the requests with no origin (like curl requests, mobile apps, etc )
-        if (!origin) return callback(null, true);
-
-        if (allowedDomains.indexOf(origin) === -1) {
-          const msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
-          return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-      }
-    })
-  );
+  app.use(cors());
 };
