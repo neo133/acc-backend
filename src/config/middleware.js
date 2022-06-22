0ce009d84bb7bb68f659ae6329aca80fe49eb402
Akshay Chaturvedi <akshay.chaturvedi@vaahaq.com>
2022-06-22 09:44:30 +0530

    remove cors policies

--- src/config/middleware.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import cors from 'cors';
-
-// const allowedDomains = ['http://localhost:3000'];
-
-export default app => {
-  app.use(express.json());
-  app.use(cors());
-};
--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -10,28 +10,11 @@ import cookieParser from 'cookie-parser';
 import winstonInstance from './winston';
 import constants from './constants';
 
-const allowedDomains = [constants.ALLOWED_DOMAIN];
-
 export default app => {
   app.use(compression());
   app.use(express.json());
   app.use(helmet());
-  app.use(
-    cors({
-      credentials: true,
-      exposedHeaders: ['Authorization'],
-      origin: (origin, callback) => {
-        // bypass the requests with no origin (like curl requests, mobile apps, etc )
-        if (!origin) return callback(null, true);
-
-        if (allowedDomains.indexOf(origin) === -1) {
-          const msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
-          return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-      }
-    })
-  );
+  app.use(cors());
   app.use(cookieParser());
   app.use(passport.initialize());
   app.use(passport.session());
